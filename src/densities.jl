95d75e2f5f6a5812dd6f12dcd70c0f37a994d797
Abhro R <5664668+abhro@users.noreply.github.com>
2024-05-07 16:01:27 -0400

    Add docstrings for density models

--- src/densities.jl
+++ src/densities.jl
@@ -1,12 +1,43 @@
 # SPDX-License-Identifier: MIT
 
+"""
+    MassDensityModel
+
+Abstract type for all mass density models. Any subtype of `MassDensityModel`
+should have a corresponding [`mass_density`](@ref) implementation.
+"""
 abstract type MassDensityModel end
 
+"""
+    UniformSphereDensity <: MassDensityModel
+
+Model for a uniform density sphere centered at the origin.
+
+# Fields
+- `radius`: radius of the sphere
+- `density`: density of the sphere
+
+See also [the implementation of `mass_density` for this type](@ref mass_density(::UniformSphereDensity, ::Any, ::Any, ::Any)).
+"""
 struct UniformSphereDensity <: MassDensityModel
     radius::Float64
     density::Float64
 end
 
+"""
+    PowerLawSphereDensity <: MassDensityModel
+
+Model for a sphere with a power law density centered at the origin.
+
+# Fields
+- `radius`: radius of the sphere
+- `scale_density`: scale density of the sphere, density changes by
+   `scale_density`^`alpha` at `radius` = `scale_radius`
+- `scale_radius`: scale radius of the sphere
+- `alpha`: power by which density reduces with respect to radius
+
+See also [the implementation of `mass_density` for this type](@ref mass_density(::PowerLawSphereDensity, ::Any, ::Any, ::Any)).
+"""
 struct PowerLawSphereDensity <: MassDensityModel
     radius::Float64
     scale_density::Float64
@@ -14,12 +45,42 @@ struct PowerLawSphereDensity <: MassDensityModel
     alpha::Float64
 end
 
+"""
+    UniformCylinderDensity <: MassDensityModel
+
+Model for a uniform density cylinder centered at the origin.
+
+# Fields
+- `radius`: radius of the cylinder
+- `height`: height of the cylinder
+- `density`: density of the cylinder
+
+See also [the implementation of `mass_density` for this type](@ref mass_density(::UniformCylinderDensity, ::Any, ::Any, ::Any)).
+"""
 struct UniformCylinderDensity <: MassDensityModel
     radius::Float64
     height::Float64
     density::Float64
 end
 
+"""
+    SpiralGalaxyDensity <: MassDensityModel
+
+Model for a spiral galaxy that has both a disk and a central bulge,
+with the bulge density dominating in the overlap.
+
+The bulge is modeled as a uniform density sphere,
+and the disk is modeled as a uniform density cylinder.
+
+# Fields
+- `bulge_radius`: radius of the bulge
+- `disk_radius`: radius of the disk
+- `disk_height`: height of the disk
+- `bulge_density`: density of the bulge
+- `disk_density`: density of the cylinder
+
+See also [the implementation of `mass_density` for this type](@ref mass_density(::SpiralGalaxyDensity, ::Any, ::Any, ::Any)).
+"""
 struct SpiralGalaxyDensity <: MassDensityModel
     bulge_radius::Float64
     disk_radius::Float64
@@ -30,6 +91,28 @@ struct SpiralGalaxyDensity <: MassDensityModel
 end
 
 
+"""
+    mass_density(model::MassDensityModel, s, φ, z)
+
+Given a mass density profile/model, return the corresponding mass density at
+cylindrical coordinates ``(s, φ, z)``.
+"""
+function mass_density end
+
+@doc raw"""
+    mass_density(model::UniformSphereDensity, s, φ, z)
+
+Density model
+```math
+ρ(s, φ, z) = \begin{cases}
+    ρ_s & \text{if } \sqrt{s^2 + z^2} ≤ R_s \\
+    0 & \text{otherwise}
+\end{cases}
+```
+where
+- ``ρ_s`` = `model.density`
+- ``R_s`` = `model.radius`
+"""
 function mass_density(model::UniformSphereDensity, s, φ, z)
     rad = hypot(s, z)
     if rad ≤ model.radius
@@ -38,6 +121,22 @@ function mass_density(model::UniformSphereDensity, s, φ, z)
     return 0.0
 end
 
+@doc raw"""
+    mass_density(model::PowerLawSphereDensity, s, φ, z)
+
+Density model
+```math
+ρ(s, φ, z) = \begin{cases}
+    ρ_0 \left(\sqrt{s^2 + z^2}/R_0\right)^α & \text{if } \sqrt{s^2 + z^2} ≤ R_s \\
+    0 & \text{otherwise}
+\end{cases}
+```
+where
+- ``ρ_0`` = `model.scale_density`
+- ``R_0`` = `model.scale_radius`
+- ``α`` = `model.alpha`
+- ``R_s`` = `model.radius`
+"""
 function mass_density(model::PowerLawSphereDensity, s, φ, z)
     rad = hypot(s, z)
     if rad ≤ model.radius
@@ -46,6 +145,21 @@ function mass_density(model::PowerLawSphereDensity, s, φ, z)
     return 0.0
 end
 
+@doc raw"""
+    mass_density(model::UniformCylinderDensity, s, φ, z)
+
+Density model
+```math
+ρ(s, φ, z) = \begin{cases}
+    ρ_0 & \text{if } s ≤ R_c, z ≤ H_c \\
+    0 & \text{otherwise}
+\end{cases}
+```
+where
+- ``ρ_0`` = `model.density`
+- ``R_c`` = `model.radius`
+- ``H_c`` = `model.height`
+"""
 function mass_density(model::UniformCylinderDensity, s, φ, z)
     if s ≤ model.radius && z ≤ model.height
         return model.density
@@ -53,6 +167,24 @@ function mass_density(model::UniformCylinderDensity, s, φ, z)
     return 0.0
 end
 
+@doc raw"""
+    mass_density(model::SpiralGalaxyDensity, s, φ, z)
+
+Density model
+```math
+ρ(s, φ, z) = \begin{cases}
+    ρ_b & \text{if } \sqrt{s^2 + z^2} ≤ R_b \\
+    ρ_d & \text{if } s ≤ R_d, |z| ≤ H_d \\
+    0 & \text{otherwise}
+\end{cases}
+```
+where
+- ``ρ_b`` = `model.bulge_density`
+- ``ρ_d`` = `model.disk_density`
+- ``R_b`` = `model.bulge_radius`
+- ``R_d`` = `model.disk_radius`
+- ``H_d`` = `model.disk_height`
+"""
 function mass_density(model::SpiralGalaxyDensity, s, φ, z)
     rad = hypot(s, z)
 
@@ -65,5 +197,3 @@ function mass_density(model::SpiralGalaxyDensity, s, φ, z)
 
     return 0.0
 end
-
-function mass_density end
