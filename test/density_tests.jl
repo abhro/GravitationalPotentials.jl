981194eb7c9f11b33cb27ada211174f59429fdbf
Abhro R <5664668+abhro@users.noreply.github.com>
2024-05-08 13:31:25 -0400

    Add todo comments for tests

--- test/density_tests.jl
+++ test/density_tests.jl
@@ -7,14 +7,53 @@ using Test
 
     @testset "uniform sphere" begin
 
+        # TODO create model
+
+        # TODO test at origin
+
+        # TODO test at half radius
+
+        # TODO test at random point between 0 and half radius
+
+        # TODO test at random point between half and full radius
+
+        # TODO test at radius/surface boundary
+
+        # TODO test at 1.1 radius
+
+        # TODO test at 2 radius
+
+        # TODO test at infinity
+
     end
 
     @testset "uniform cylinder" begin
+
+        # TODO create model
+
+        # TODO test at roof
+
+        # TODO test at floor
+
+        # TODO test at walls
+
+        # TODO test at origin
 
     end
 
     @testset "power law sphere" begin
 
+        # TODO create model with scale radius == sphere radius
+        # TODO create model with scale radius < sphere radius
+        # TODO create model with scale radius > sphere radius
+
+
+        # TODO create model with α < -1
+        # TODO create model with α = -1
+        # TODO create model with α ∈ (-1, 0)
+        # TODO create model with α ∈ (0, 1)
+        # TODO create model with α = 1
+        # TODO create model with α > 1
     end
 
     @testset "spiral galaxy" begin
