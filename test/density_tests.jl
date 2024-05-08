f3bafbed4802e9894f703c2379b8f23b2288bab7
Abhro R <5664668+abhro@users.noreply.github.com>
2024-05-08 11:57:54 -0400

    Add test file for density models

---
+++ test/density_tests.jl
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: MIT
+
+using GravitationalPotentials
+using Test
+
+@testset "density models" begin
+
+    @testset "uniform sphere" begin
+
+    end
+
+    @testset "uniform cylinder" begin
+
+    end
+
+    @testset "power law sphere" begin
+
+    end
+
+    @testset "spiral galaxy" begin
+
+    end
+
+    @testset "Einasto galaxy" begin
+
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
+# SPDX-License-Identifier: MIT
+
 using GravitationalPotentials
 using Test
+
+include("density_tests.jl")
 
 @testset "GravitationalPotentials.jl" begin
     # Write your tests here.
