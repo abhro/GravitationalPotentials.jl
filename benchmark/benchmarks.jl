1d964f5ef64562df0b3c7c37f6ac9bfc97eb8861
Abhro R <5664668+abhro@users.noreply.github.com>
2024-12-25 19:06:18 -0500

    Remove benchmarks directory

--- benchmark/benchmarks.jl
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: MIT
-
-using GravitationalPotentials
-using BenchmarkTools
-
-SUITE = BenchmarkGroup()
-SUITE["rand"] = @benchmarkable rand(10)
-
-# Write your benchmarks here.
