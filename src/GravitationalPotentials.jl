dedfda7b631c0d249de2c34afc7930f55a501b2d
Abhro R <5664668+abhro@users.noreply.github.com>
2024-12-25 19:09:29 -0500

    Add bounds method

--- src/GravitationalPotentials.jl
+++ src/GravitationalPotentials.jl
@@ -3,6 +3,7 @@
 module GravitationalPotentials
 
 include("densities.jl")
+include("potentials.jl")
 
 export mass_density
 export MassDensityModel
@@ -14,4 +15,18 @@ export SpiralGalaxyDensity
 export EinastoDensity
 
 
+"""Cylindrical coordinates bounds `[(s_lo, s_hi), (φ_lo, φ_hi), (z_lo, z_hi)]`"""
+function bounds end
+export bounds
+
+"""Cylindrical coordinates bounds `([s_lo, φ_lo, z_lo], [s_hi, φ_hi, z_hi])`"""
+function bounds_t(model::MassDensityModel)
+    r_bounds, φ_bounds, z_bounds = bounds(model)
+    return ([r_bounds[1], φ_bounds[1], z_bounds[1]], [r_bounds[2], φ_bounds[2], z_bounds[2]])
+end
+export bounds_t
+
+
+export mass
+export potential
 end
