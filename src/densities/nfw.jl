68929479b301cef264c2d45e5b0391be4d1504e7
Abhro R <5664668+abhro@users.noreply.github.com>
2024-12-25 19:07:23 -0500

    Create NFW profile model

---
+++ src/densities/nfw.jl
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: MIT
+
+"""
+    NFWDensity <: MassDensityModel
+
+# Fields
+$(TYPEDFIELDS)
+"""
+struct NFWDensity <: MassDensityModel
+    "Scale density"
+    ρ₀::Float64
+    "Scale radius"
+    r_s::Float64
+end
+
+@doc raw"""
+    mass_density(model::NFWDensity, s, φ, z)
+
+Density model
+```math
+ρ(s, φ, z) = \frac{ρ₀}{\frac{r}{r_s} \left(1 + \frac{r}{r_s}\right)^2}
+```
+where ``r = \sqrt{s^2 + z^2}``
+"""
+function mass_density(model::NFWDensity, s, φ, z)
+    r = hypot(s, z)
+    return model.ρ₀ / ((r/model.r_s) * (1 + r/model.r_s)^2)
+end
+
+function bounds(model::NFWDensity)
+    return [(0, Inf), (0, 2π), (-Inf, Inf)]
+end
