3673cf313afa452feab0ef0612fce2e17da7986a
abhro <5664668+abhro@users.noreply.github.com>
2025-03-25 11:50:01 -0400

    Aqq aqua tests
    Signed-off-by: abhro <5664668+abhro@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,12 @@
 
 using GravitationalPotentials
 using Test
-
+using Aqua
 
 @testset "GravitationalPotentials.jl" begin
     include("density_tests.jl")
+
+    @testset "Code quality (Aqua)" begin
+        Aqua.test_all(GravitationalPotentials)
+    end
 end
