2cf4cedf12979fa6dc509c3ada772f2afe9ea0f0
abhro <5664668+abhro@users.noreply.github.com>
2025-03-25 12:27:53 -0400

    Update coordinate math

--- src/coordinate_utils.jl
+++ src/coordinate_utils.jl
@@ -1,74 +1,101 @@
 # SPDX-License-Identifier: MIT
 
 import LinearAlgebra
-using CoordinateTransformations
+
+nonnegative(x::Real) = x < 0 && throw(DomainError(x, "Value must be ≥ 0"))
+positive(x::Real) = x ≤ 0 && throw(DomainError(x, "Value must be > 0"))
+
+# follow convention of Griffth's Electrodynamics when naming coordinates
+struct Polar{T<:Real,A<:Real}
+    s::T
+    φ::A
+    function Polar{T,A}(s::T, φ::A) where {T<:Real,A<:Real}
+        nonnegative(s)
+        return new{T,A}(s, mod2pi(φ))
+    end
+end
+struct Cylindrical{T<:Real,A<:Real}
+    s::T
+    φ::A # azimuthal angle
+    z::T
+    function Cylindrical{T,A}(s::T, φ::A, z::T) where {T<:Real,A<:Real}
+        nonnegative(s)
+        return new{T,A}(s, mod2pi(φ), z)
+    end
+end
+struct Spherical{T<:Real,A<:Real}
+    r::T
+    θ::A # polar angle
+    φ::A # azimuthal angle
+    function Cylindrical{T,A}(r::T, θ::A, φ::T) where {T<:Real,A<:Real}
+        nonnegative(r)
+        return new{T,A}(r, mod2pi(θ), mod2pi(φ))
+    end
+end
 
 ## Plane-polar coordinates
 
-LinearAlgebra.norm(x::Polar) = x.r
-Base.:-(x::Polar) = Polar(x.r, mod2pi(x.θ + π))
+LinearAlgebra.norm(x::Polar) = x.s
+Base.:-(x::Polar) = Polar(x.s, mod2pi(x.φ + π))
 
 function Base.:+(x₁::Polar, x₂::Polar)
-    r₁, θ₁, r₂, θ₂ = x₁.r, x₁.θ, x₂.r, x₂.θ
+    s₁, φ₁, s₂, φ₂ = x₁.s, x₁.φ, x₂.s, x₂.φ
 
-    rsum = sqrt(r₁^2 + r₂^2 - 2*r₁*r₂*cos(θ₁-θ₂))
+    s = sqrt(s₁^2 + s₂^2 - 2*s₁*s₂*cos(φ₁-φ₂))
     # TODO simplify/expand?
-    θsum = atan(r₁*sin(θ₁) + r₂*sin(θ₂), r₁*cos(θ₁) + r₂*cos(θ₂))
+    φ = atan(s₁*sin(φ₁) + s₂*sin(φ₂), s₁*cos(φ₁) + s₂*cos(φ₂))
 
-    return Polar(rsum, θsum)
+    return Polar(s, φ)
 end
 
 function Base.:-(x₁::Polar, x₂::Polar)
-    r₁, θ₁, r₂, θ₂ = x₁.r, x₁.θ, x₂.r, x₂.θ
+    s₁, φ₁, s₂, φ₂ = x₁.s, x₁.φ, x₂.s, x₂.φ
 
-    rdiff = sqrt(r₁^2 + r₂^2 + 2*r₁*r₂*cos(θ₁-θ₂))
+    s = sqrt(s₁^2 + s₂^2 + 2*s₁*s₂*cos(φ₁-φ₂))
     # TODO simplify/expand?
-    θdiff = atan(r₁*sin(θ₁) - r₂*sin(θ₂), r₁*cos(θ₁) - r₂*cos(θ₂))
+    φdiff = atan(s₁*sin(φ₁) - s₂*sin(φ₂), s₁*cos(φ₁) - s₂*cos(φ₂))
 
-    return Polar(rdiff, θdiff)
+    return Polar(s, φdiff)
 end
 
-Base.:*(c::Number, x::Polar) = Polar(c * x.r, x.θ)
-Base.:*(x::Polar, c::Number) = Polar(x.r * c, x.θ)
+Base.:*(c::Real, x::Polar) = Polar(abs(c) * x.s, x.φ + π*(c<0))
+Base.:*(x::Polar, c::Real) = Polar(x.s * abs(c), x.φ + π*(c<0))
 
-Base.:/(x::Polar, c::Number) = Polar(x.r / c, x.θ)
+Base.:/(x::Polar, c::Real) = Polar(x.s / abs(c), x.φ + π*(c<0))
 
 
 ## Cylindrical coordinates
 
-LinearAlgebra.norm(x::Cylindrical) = hypot(x.r, x.z)
-Base.:-(x::Cylindrical) = Cylindrical(x.r, mod2pi(x.θ + π), -x.z)
+LinearAlgebra.norm(x::Cylindrical) = hypot(x.s, x.z)
+Base.:-(x::Cylindrical) = Cylindrical(x.s, mod2pi(x.φ + π), -x.z)
 
 function Base.:+(x₁::Cylindrical, x₂::Cylindrical)
-    r₁, θ₁, z₁, r₂, θ₂, z₂ = x₁.r, x₁.θ, x₁.z, x₂.r, x₂.θ, x₂.z
+    s₁, φ₁, z₁, s₂, φ₂, z₂ = x₁.s, x₁.φ, x₁.z, x₂.s, x₂.φ, x₂.z
 
-    rsum = sqrt(r₁^2 + r₂^2 - 2*r₁*r₂*cos(θ₁-θ₂))
+    ssum = sqrt(s₁^2 + s₂^2 - 2*s₁*s₂*cos(φ₁-φ₂))
     # TODO simplify/expand?
-    θsum = atan(r₁*sin(θ₁) + r₂*sin(θ₂), r₁*cos(θ₁) + r₂*cos(θ₂))
+    φsum = atan(s₁*sin(φ₁) + s₂*sin(φ₂), s₁*cos(φ₁) + s₂*cos(φ₂))
     zsum = z₁ + z₂
 
-    return Cylindrical(rsum, θsum, zsum)
+    return Cylindrical(ssum, φsum, zsum)
 end
 
 function Base.:-(x₁::Cylindrical, x₂::Cylindrical)
-    r₁, θ₁, z₁, r₂, θ₂, z₂ = x₁.r, x₁.θ, x₁.z, x₂.r, x₂.θ, x₂.z
+    s₁, φ₁, z₁, s₂, φ₂, z₂ = x₁.s, x₁.φ, x₁.z, x₂.s, x₂.φ, x₂.z
 
-    rdiff = sqrt(r₁^2 + r₂^2 + 2*r₁*r₂*cos(θ₁-θ₂))
+    sdiff = sqrt(s₁^2 + s₂^2 + 2*s₁*s₂*cos(φ₁-φ₂))
     # TODO simplify/expand?
-    θdiff = atan(r₁*sin(θ₁) - r₂*sin(θ₂), r₁*cos(θ₁) - r₂*cos(θ₂))
+    φdiff = atan(s₁*sin(φ₁) - s₂*sin(φ₂), s₁*cos(φ₁) - s₂*cos(φ₂))
     zdiff = z₁ - z₂
 
-    return Cylindrical(rdiff, θdiff, zdiff)
+    return Cylindrical(sdiff, φdiff, zdiff)
 end
 
 # the +π trick is to take care of negative c
-Base.:*(c::Real, x::Cylindrical) = Cylindrical(
-    abs(c) * x.r, mod2pi(x.θ + π*(c<0)), c * x.z)
-Base.:*(x::Cylindrical, c::Real) = Cylindrical(
-    x.r * abs(c), mod2pi(x.θ + π*(c<0)), x.z * c)
+Base.:*(c::Real, x::Cylindrical) = Cylindrical(abs(c) * x.s, x.φ + π*(c<0), c * x.z)
+Base.:*(x::Cylindrical, c::Real) = Cylindrical(x.s * abs(c), x.φ + π*(c<0), x.z * c)
 
-Base.:/(x::Cylindrical, c::Real) = Cylindrical(
-    x.r / abs(c), mod2pi(x.θ + π * (c < 0)), x.z / c)
+Base.:/(x::Cylindrical, c::Real) = Cylindrical(x.s / abs(c), x.φ + π*(c<0), x.z / c)
 
 
 ## Spherical coordinates
