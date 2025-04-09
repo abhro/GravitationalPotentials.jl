5894b61b0e2a9acc7fa9b875b26e15212ed4b0eb
abhro <5664668+abhro@users.noreply.github.com>
2025-04-09 09:11:22 -0400

    Create test/potential_tests.jl

---
+++ test/potential_tests.jl
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: MIT
+
+using GravitationalPotentials
+using Test
+using Unitful, UnitfulAstro
+
+
+@testset "Potential calculations" begin
+
+    # this has analytic solutions
+    @testset "Uniform sphere density model" begin
+    end
+
+    @testset "uniform cylinder" begin
+
+        # TODO create model
+
+        # TODO test at roof
+
+        # TODO test at floor
+
+        # TODO test at walls
+
+        # TODO test at origin
+
+    end
+end
