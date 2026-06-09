40c26cc3250e192d45362415c89ab2420f67191f
abhro <5664668+abhro@users.noreply.github.com>
2026-06-09 10:07:59 +0600

    More code

--- src/integrators.jl
+++ src/integrators.jl
@@ -10,11 +10,15 @@ function integrate_disk(f, s1, s2)
     I = quadgk(s -> f(s) * s, s1, s2) |> first
     return 2π * I
 end
-integrate_disk(f, s) = integrate_disk(f, zero(s), s)
+integrate_disk(f, S) = integrate_disk(f, zero(S), S)
 
 """
 $(SIGNATURES)
 
-Integrate a function ``f(s, z)`` over a cylinder.
+Integrate a function ``f(s, z)`` over a cylinder. Assumes azimuthal symmetry.
 """
-function integrate_cylinder(f, s2, z1, z2)
+function integrate_cylinder(f, S, z1, z2)
+    z_integrand = z -> first(quadgk(s -> s * f(s, z), zero(S), S))
+    I, resid = quadgk(z_integrand, z1, z2)
+    return 2π * I
+end
